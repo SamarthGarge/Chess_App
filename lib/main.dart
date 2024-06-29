d8daf529c126bebdfd50704e76812c5a3b93b23c
Samarth Garge <gargesamarth@gmail.com>
2024-06-29 19:39:14 +0530

    firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chess/authentication/login_screen.dart';
 import 'package:flutter_chess/authentication/sign_up_screen.dart';
 import 'package:flutter_chess/constants.dart';
+import 'package:flutter_chess/firebase_options.dart';
 import 'package:flutter_chess/main_screens/about_screen.dart';
 import 'package:flutter_chess/main_screens/game_screen.dart';
 import 'package:flutter_chess/main_screens/game_time_screen.dart';
@@ -10,13 +12,16 @@ import 'package:flutter_chess/main_screens/settings_screen.dart';
 import 'package:flutter_chess/providers/game_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (_) => GameProvider())
-  ],
-    
-    child: const MyApp()),
-    );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(
+    MultiProvider(
+        providers: [ChangeNotifierProvider(create: (_) => GameProvider())],
+        child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -46,4 +51,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
